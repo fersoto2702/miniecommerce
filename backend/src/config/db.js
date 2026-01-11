ea21d9a785a72a5fce060a4a6f8589eefe23153f
fersoto2702 <fernandodaniel97@outlook.com>
2026-01-11 01:07:14 -0700

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT || 'mysql',
+    dialect: 'mysql',
     logging: false,
   }
 );
